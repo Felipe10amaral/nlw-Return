17e2a58c8908258498bff4c41bd21ecb07891006
Felipe Amaral <felipe.sf2011@gmail.com>
2022-05-04 23:15:13 -0300

    criacao de componentes

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
-import { ArrowLeft } from 'phosphor-react';
+import { ArrowLeft, Camera } from 'phosphor-react';
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
+import { ScreenshotButton } from '../ScreenshotButton';
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -31,9 +32,19 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: 
             <form className='my-4 w-full'>
                 <textarea 
                     placeholder='Conte com detalhes o que está acontecendo... '
-                    className='min-w[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md'
+                    className='min-w[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin '
                 />
 
+                <footer className='flex gap-2 mt-2'>
+                    <ScreenshotButton />
+
+                    <button
+                        type="submit"
+                        className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
+                    >
+                        Enviar Feedback
+                    </button>
+                </footer>
             </form>
         </>    
     )
