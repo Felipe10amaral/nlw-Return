5ca9a6f4c3102e0967d67475e93f8ca05341e6ed
Felipe Amaral <felipe.sf2011@gmail.com>
2022-05-05 23:28:11 -0300

    configuração de envio de email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
@@ -7,7 +8,16 @@ app.use(express.json())
 
 app.listen(3330, () => {
     console.log("Running Server");
-})
+});
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "6a5d0a2ec830d8",
+      pass: "8b129e99a73123"
+    }
+  });
 
 app.post('/feedbacks', async (request, response) => {
     const {type, comment, screenshot} = request.body;
@@ -18,6 +28,16 @@ app.post('/feedbacks', async (request, response) => {
             comment,
             Screenshot: screenshot
         }
+    })
+
+    await transport.sendMail({
+        from: "Equipe feedget <oi@feedget.com>",
+        to: "Felipe <felipeamaraldev15@gmail.com>",
+        subject: "Assunto do email",
+        html: [
+            `<p> Tipo do feedback ${type}</p>`,
+            `<p> Comentario ${comment}</p>`
+        ].join('\n')
     })
 
     return response.status(201).json(feedback);
