d6db4302682e17598d231cef709ed29d8684498b
Felipe Amaral <felipe.sf2011@gmail.com>
2022-05-05 23:41:41 -0300

    organizando a aplicação

---
+++ server/src/routes.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
+
+export const router = express.Router();
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "6a5d0a2ec830d8",
+      pass: "8b129e99a73123"
+    }
+  });
+
+router.post('/feedbacks', async (request, response) => {
+    const {type, comment, screenshot} = request.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            Screenshot: screenshot
+        }
+    })
+
+    await transport.sendMail({
+        from: "Equipe feedget <oi@feedget.com>",
+        to: "Felipe <felipeamaraldev15@gmail.com>",
+        subject: "Assunto do email",
+        html: [
+            `<p> Tipo do feedback ${type}</p>`,
+            `<p> Comentario ${comment}</p>`
+        ].join('\n')
+    })
+
+    return response.status(201).json(feedback);
+})
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
-import { prisma } from './prisma';
-import nodemailer from 'nodemailer';
+
+
+import { router } from './routes';
 
 const app = express();
 
@@ -10,35 +11,6 @@ app.listen(3330, () => {
     console.log("Running Server");
 });
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "6a5d0a2ec830d8",
-      pass: "8b129e99a73123"
-    }
-  });
 
-app.post('/feedbacks', async (request, response) => {
-    const {type, comment, screenshot} = request.body;
 
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            Screenshot: screenshot
-        }
-    })
-
-    await transport.sendMail({
-        from: "Equipe feedget <oi@feedget.com>",
-        to: "Felipe <felipeamaraldev15@gmail.com>",
-        subject: "Assunto do email",
-        html: [
-            `<p> Tipo do feedback ${type}</p>`,
-            `<p> Comentario ${comment}</p>`
-        ].join('\n')
-    })
-
-    return response.status(201).json(feedback);
-})
+app.use(router);
