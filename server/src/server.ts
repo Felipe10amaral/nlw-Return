d8ab0bfdd4499ee5f0bbf3d7399f7e875e2db0fe
Felipe Amaral <felipe.sf2011@gmail.com>
2022-05-05 23:10:06 -0300

    criacao da rota de cadastro de feedbacks

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,24 @@
-import express from 'express';
+import express, { json } from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(3330, () => {
     console.log("Running Server");
+})
+
+app.post('/feedbacks', async (request, response) => {
+    const {type, comment, screenshot} = request.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            Screenshot: screenshot
+        }
+    })
+
+    return response.status(201).json(feedback);
 })
