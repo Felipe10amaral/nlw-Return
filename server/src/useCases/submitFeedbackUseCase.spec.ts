291995daa1ccdbc22124d70b5ec28dead37ca4c4
Felipe Amaral <felipe.sf2011@gmail.com>
2022-05-06 01:27:56 -0300

    adicionando cors na aplicacao

--- server/src/useCases/submitFeedbackUseCase.spec.ts
+++ server/src/useCases/submitFeedbackUseCase.spec.ts
@@ -1,3 +1,16 @@
-test('sum 2+2 ', ()=> {
-    expect(2 + 2).toBe(4)
-});
+import { SubmitFeedbackUseCases } from "./submitFeedbackUseCases"
+
+describe('Submit feedback', () => {
+    it('shold be able to submit a feedback ', async () =>{
+        const submitFeedback = new SubmitFeedbackUseCases(
+            { create: async () => {} },
+            { sendMail: async () => {} },
+        )
+
+        await expect(submitFeedback.execute({
+            comment: "bugado",
+            type: "BUG",
+            screenshot: "test.jpg"
+        })).resolves.not.toThrow();
+    })
+})
